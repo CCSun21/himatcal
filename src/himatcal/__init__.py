7404642e0474ba4f0904d9e339aae6a49744a093
Congcong Sun <suncc@hithium.cn>
2024-06-17 19:22:46 +0800

    test release

--- src/himatcal/__init__.py
+++ src/himatcal/__init__.py
@@ -1,8 +1,13 @@
-"""Init data"""
+"""Init data for himatcal package."""
 
 from __future__ import annotations
 
 from importlib.metadata import version
 
-# Load the version
+from himatcal.settings import HimatcalSettings
+
+# load the version from the pyproject.toml file
 __version__ = version("himatcal")
+
+# load the settings
+SETTINGS = HimatcalSettings()
