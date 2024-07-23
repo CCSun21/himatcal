adced2e51c254ba8ac609286e85de57034a3a375
Congcong Sun <suncc@hithium.cn>
2024-07-23 20:16:00 +0800

    chore: Update visualization.py with import statements and style initialization

--- src/himatcal/utils/visualization.py
+++ src/himatcal/utils/visualization.py
@@ -1,16 +1,31 @@
 """ functions using for visualization """
+from __future__ import annotations
 
-def view_atoms(atoms, format='xyz'):
+from pathlib import Path
+
+
+def view_atoms(atoms, fmt="xyz"):
     """
-    View the atoms in jupyter notebook using py3Dmol.
+    View atoms using a 3D molecular viewer.
+
+    Args:
+        atoms: The molecular structure to visualize.
+        fmt: The format in which to save the molecular structure (default is "xyz").
+
+    Returns:
+        A 3D molecular viewer displaying the atoms.
+
+    Raises:
+        FileNotFoundError: If the temporary file "tmp_atoms" is not found.
     """
+
     import py3Dmol
-    from ase.io import read, write
-    write('tmp_atoms', atoms, format=format)
-    atoms_data = open('tmp_atoms', 'r').read()
+    from ase.io import write
+    write("tmp_atoms", atoms, format=fmt)
+    atoms_data = Path.open("tmp_atoms").read()
     view = py3Dmol.view(width=800, height=400)
     view.addModel(atoms_data, format)
-    view.setStyle({'stick':{}})
+    view.setStyle({"stick":{}})
     view.zoomTo()
     return view
 
@@ -22,11 +37,11 @@ def show_xyz_mol(xyz_file):
     mol = open(xyz_file).read()
     p = py3Dmol.view(
         data = mol,
-        style = {'stick': {'colorscheme': 'Jmol'}},
+        style = {"stick": {"colorscheme": "Jmol"}},
         width=400,
         height=400,
     )
-    p.setBackgroundColor('white')
+    p.setBackgroundColor("white")
     p.zoomTo()
     p.show()
 
@@ -36,22 +51,22 @@ def xyz_to_mol(xyz_file, write_mol=True):
     """
     from openbabel import pybel as pb
     # ! openbabel is a conda package, try other packages if openbabel is not available.
-    mol = next(pb.readfile('xyz', xyz_file))
+    mol = next(pb.readfile("xyz", xyz_file))
     if write_mol:
-        mol.write('mol', f'{xyz_file}.mol', overwrite=True)
-        return open(f'{xyz_file}.mol').read()
+        mol.write("mol", f"{xyz_file}.mol", overwrite=True)
+        return open(f"{xyz_file}.mol").read()
 
 # * Gasiteger charge visualization
 def plot_gasteiger_charges(mol):
     """
     Plot Gasteiger charges on a molecule.
     """
-    from rdkit.Chem.Draw import SimilarityMaps
     from rdkit.Chem import AllChem
+    from rdkit.Chem.Draw import SimilarityMaps
     AllChem.ComputeGasteigerCharges(mol)
-    contribs = [float(mol.GetAtomWithIdx(i).GetProp('_GasteigerCharge')) for i in range(mol.GetNumAtoms())]
+    contribs = [float(mol.GetAtomWithIdx(i).GetProp("_GasteigerCharge")) for i in range(mol.GetNumAtoms())]
     SimilarityMaps.GetSimilarityMapFromWeights(
-        mol, contribs, colorMap='jet', contourLines=10
+        mol, contribs, colorMap="jet", contourLines=10
     )
 
 def init_style():
@@ -62,6 +77,6 @@ def init_style():
         import matplotlib.pyplot as plt
         import pkg_resources
         plt.style.use(pkg_resources.resource_filename("himatcal", "tools/science-1.mplstyle"))
-        plt.rcParams['font.family'] ='Calibri, Microsoft YaHei'
+        plt.rcParams["font.family"] ="Calibri, Microsoft YaHei"
     except ImportError:
-        print('matplotlib is not installed.')
+        print("matplotlib is not installed.")
